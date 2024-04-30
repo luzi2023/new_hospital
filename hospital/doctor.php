db20703ca652eaf70ae20675a41bbfaa9f3ba53f
minion0713 <134852445+minion0713@users.noreply.github.com>
2024-04-30 18:55:44 +0800

    has pencil

--- hospital/doctor.php
+++ hospital/doctor.php
@@ -7,20 +7,20 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="styles.css" />
-
+    <script src="https://kit.fontawesome.com/4bfa319983.js" crossorigin="anonymous"></script>
     <title>Local hospital</title>
 </head>
 
 <body>
     <div class="container-fluid">
-        <div id="login">
+        <!-- <div id="login">
             <form method="post" action="login.php">User:
                 <input type="text" name="username">Password:
                 <input type="text" name="password">
                 <input type="submit" value="login" name="submit">
                 <a href="register.html">register now</a>
             </form>
-        </div>
+        </div> -->
         <div id="side-nav" class="sidenav">
             <a href="index.php" id="home">Home</a>
             <a href="doctor.php" id="doctors">Doctors</a>
@@ -28,10 +28,10 @@
             <a href="" id="about">About</a>
         </div>
 
-        <div id="body">
+        <div id="list_body">
             <h1>Doctor List</h1>
             <div class="doctor_menu">
-                <a href="add_doctor.php">Add doctor</a>
+                <a href="add_doctor.php"><i class="fa-solid fa-pencil"></i>Add doctor</a>
             </div>
 
             <?php
@@ -48,7 +48,7 @@
                         ?>
                 <a href="edit_doctor.php?dID=<?php echo $row['dID']?>">
                     <div class="current_list">
-                        <img src="<?php echo $row['dImage']; ?>" alt="Dr.<?php $row['first_name']?>'s head shot">
+                        <img src="default.jpg" alt="Dr.<?php $row['first_name']?>'s head shot">
                         <p> <?php echo $row['last_name'].', '.$row['first_name']?> </p>
                         <p> <?php echo $row['speciality'] ?> </p>
                     </div>
