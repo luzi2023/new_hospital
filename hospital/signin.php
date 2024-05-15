cf64a3f78e6f69c6db27ff026b5333465deedaa1
TingWeiChenn <165136451+TingWeiChenn@users.noreply.github.com>
2024-05-15 23:06:20 +0800

    Add files via upload

--- hospital/signin.php
+++ hospital/signin.php
@@ -1,5 +1,5 @@
 <?php
-include('config.php');
+include_once('config.php');
 
 if(isset($_POST["submit"])){
     $username = $_POST["username"];
@@ -12,22 +12,21 @@ if(isset($_POST["submit"])){
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
 
-    if($user=mysqli_fetch_assoc($result)){
-        // 验证密码是否正确
+    // 檢查是否有查詢到使用者
+    if($user = mysqli_fetch_assoc($result)){
+        // 驗證密碼是否正確
         if(password_verify($password, $user["password"])){
-            // 登录成功，重定向到 doctor.php 页面，并传递userID参数
-            echo "Login successful! userID: " . $user['userID']; // 调试信息
-            header("Location: doctor.php?userID=".$user['userID']);
+            // 登入成功，導向到醫生頁面
+            header("Location: doctor.php");
             exit();
         } else {
-            // 密码不匹配的错误消息
+            // 密碼不匹配的錯誤訊息
             $error_message = "Password does not match!";
         }
     } else {
-        // 用户名不存在的错误消息
+        // 使用者名稱不存在的錯誤訊息
         $error_message = "Username is not valid!";
     }
-    
 }
 ?>
 
@@ -37,21 +36,21 @@ if(isset($_POST["submit"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="signin.css" />
-    <script src="https://kit.fontawesome.com/4bfa319983.js" crossorigin="anonymous"></script>
     <title>Local hospital</title>
 </head>
 
 <body>
-        <div id="login">
-            <form method="post" action="doctor.php">User:
-                <input type="text" name="username">Password:
-                <input type="password" name="password">
-                <input type="submit" value="login" name="submit">
-                <a href="register.php">register now</a>
-            </form>
-        </div>
+    <div id="login">
+        <form method="post" action="signin.php">
+            User:
+            <input type="text" name="username">Password:
+            <input type="password" name="password">
+            <input type="submit" value="Login" name="submit">
+            <a href="register.php">Register now</a>
+            <?php if(isset($error_message)) { echo "<p class='error'>$error_message</p>"; } ?>
+        </form>
+    </div>
 </body>
+
 </html>
