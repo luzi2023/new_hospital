292f764f0162c22d416c5e3d42ff7728d1fc5414
luzi2023 <104833887+luzi2023@users.noreply.github.com>
2024-05-22 14:36:07 +0800

    Add files via upload

--- hospital/treatment.php
+++ hospital/treatment.php
@@ -48,89 +48,88 @@ include('config.php');
         </div>
         <h1>Treatment Option</h1>
         <!-- search front, back end -->
-        <form action="treatment_search.php">
-            <div class="dropdown">
-                <button onclick="myFunction()" class="treatment-search-button">Sort in</button>
-                <div id="myDropdown" class="dropdown-content">
-                    <p onclick="sort('most_used')">The most used</p>
-                    <p onclick="sort('highest_price')">Highest Price</p>
-                    <p onclick="sort('default')">Default</p>
-                </div>
+        <div class="dropdown">
+            <button onclick="myFunction()" class="treatment-search-button">Sort in</button>
+            <div id="myDropdown" class="dropdown-content">
+                <p onclick="sort('most_used')">The most used</p>
+                <p onclick="sort('highest_price')">Highest Price</p>
+                <p onclick="sort('default')">Default</p>
             </div>
-            <form id="sortForm" method="GET" action="">
-                <input type="hidden" name="sort" id="sortInput">
-            </form>
-            <form action="treatment_search.php" class="button-form">
-                <label for="search"></label>
-                <input type="submit" value="Search" class="treatment-search-button">
-            </form>
+        </div>
+        <form id="sortForm" method="GET" action="">
+            <input type="hidden" name="sort" id="sortInput">
+        </form>
+        <form action="treatment_search.php" class="button-form">
+            <label for="search"></label>
+            <input type="submit" value="Search" class="treatment-search-button">
+        </form>
 
-            <!-- option list -->
-            <?php
-// Get sorting option from GET request
-$Option = isset($_GET['sort']) ? $_GET['sort'] : 'most_used';
+        <!-- option list -->
+        <?php
+        // Get sorting option from GET request
+        $Option = isset($_GET['sort']) ? $_GET['sort'] : 'most_used';
 
-// Build SQL query based on sorting option
-switch($Option) {
-case 'most_used':
-    $sort = "SELECT t.tID, t.tName, t.tType, COUNT(m.treatment) AS treatment_count
-            FROM treatment AS t
-            LEFT JOIN medical_history AS m ON m.treatment LIKE CONCAT('%', t.tName, '%')
-            GROUP BY t.tID, t.tName, t.tType
-            ORDER BY treatment_count DESC";
-    break;
-case 'highest_price':
-    $sort = "SELECT tID, tName, tType, price
-            FROM treatment
-            ORDER BY price DESC";
-    break;
-case 'default':
-    $sort = "SELECT *
-            FROM treatment";
-    break;
-}
+        // Build SQL query based on sorting option
+        switch($Option) {
+        case 'most_used':
+            $sort = "SELECT t.tID, t.tName, t.tType, COUNT(m.treatment) AS treatment_count
+                    FROM treatment AS t
+                    LEFT JOIN medical_history AS m ON m.treatment LIKE CONCAT('%', t.tName, '%')
+                    GROUP BY t.tID, t.tName, t.tType
+                    ORDER BY treatment_count DESC";
+            break;
+        case 'highest_price':
+            $sort = "SELECT tID, tName, tType, price
+                    FROM treatment
+                    ORDER BY price DESC";
+            break;
+        case 'default':
+            $sort = "SELECT *
+                    FROM treatment";
+            break;
+        }
 
-// Execute the query
-$result = mysqli_query($link, $sort);
-?>
-            <table class="treatment-table">
-                <tr>
-                    <th>tID</th>
-                    <th>tName</th>
-                    <th>tType</th>
-                    <?php if ($Option == 'most_used'): ?>
-                    <th>Number of Patients Used</th>
-                    <?php elseif ($Option == 'highest_price'): ?>
-                    <th>Price</th>
-                    <?php endif; ?>
-                </tr>
-                <div class="treatment-body-list">
-                    <?php
-if (mysqli_num_rows($result) > 0) {
-    foreach ($result as $row) {
+        // Execute the query
+        $result = mysqli_query($link, $sort);
         ?>
-                    <tr>
+        <table class="treatment-table">
+            <tr>
+                <th>tID</th>
+                <th>tName</th>
+                <th>tType</th>
+                <?php if ($Option == 'most_used'): ?>
+                <th>Number of Patients Used</th>
+                <?php elseif ($Option == 'highest_price'): ?>
+                <th>Price</th>
+                <?php endif; ?>
+            </tr>
+            <div class="treatment-body-list">
+                <?php
+                if (mysqli_num_rows($result) > 0) {
+                    foreach ($result as $row) {
+                        ?>
+                <tr>
 
-                        <td><a href="treatment_view.php?tID=<?php echo $row['tID']; ?>"><?php echo $row['tID']; ?></a>
-                        </td>
-                        <td><a href="treatment_view.php?tID=<?php echo $row['tID']; ?>"><?php echo $row['tName']; ?></a>
-                        </td>
-                        <td><a href="treatment_view.php?tID=<?php echo $row['tID']; ?>"><?php echo $row['tType']; ?></a>
-                        </td>
-                        <?php if ($Option == 'most_used'): ?>
-                        <td><?php echo $row['treatment_count']; ?></td>
-                        <?php elseif ($Option == 'highest_price'): ?>
-                        <td><?php echo $row['price']; ?></td>
-                        <?php endif; ?>
+                    <td><a href="treatment_view.php?tID=<?php echo $row['tID']; ?>"><?php echo $row['tID']; ?></a>
+                    </td>
+                    <td><a href="treatment_view.php?tID=<?php echo $row['tID']; ?>"><?php echo $row['tName']; ?></a>
+                    </td>
+                    <td><a href="treatment_view.php?tID=<?php echo $row['tID']; ?>"><?php echo $row['tType']; ?></a>
+                    </td>
+                    <?php if ($Option == 'most_used'): ?>
+                    <td><?php echo $row['treatment_count']; ?></td>
+                    <?php elseif ($Option == 'highest_price'): ?>
+                    <td><?php echo $row['price']; ?></td>
+                    <?php endif; ?>
 
-                    </tr>
-                    <?php
+                </tr>
+                <?php
             }
         } else {
             echo "<tr><td colspan='4'>No results found.</td></tr>";
         }
         ?>
-            </table>
+        </table>
     </div>
     </div>
 </body>
