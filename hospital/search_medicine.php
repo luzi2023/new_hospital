834c67d120899f8bf54ca2a64867d29b52dc3371
minion0713 <134852445+minion0713@users.noreply.github.com>
2024-05-30 13:44:36 +0800

    Update search_medicine.php

--- hospital/search_medicine.php
+++ hospital/search_medicine.php
@@ -10,10 +10,8 @@
 
 <body>
     <div class="container">
-        <!-- 返回首页的按钮 -->
         <a href="medicine.php" class="back-to-home">Back to Medication List</a>
 
-        <!-- 搜索框 -->
         <div id="aki">
             <form action="search_medicine.php" method="get">
                 <input type="text" id="query" name="query" placeholder="Search more...">
@@ -24,15 +22,11 @@
 
     <h1>Search Results</h1>
     <?php
-    // 连接数据库
     include('config.php');
 
-    // 检查是否存在关键字参数
     if (isset($_GET['query'])) {
-        // 获取用户输入的关键字并进行转义以防止 SQL 注入
         $keyword = mysqli_real_escape_string($link, $_GET['query']);
 
-        // 查询药物信息并进行分组
         $query = "
             SELECT mID, mName, mType FROM medication 
             WHERE mName LIKE '%$keyword%' 
@@ -40,12 +34,10 @@
         ";
         $result = mysqli_query($link, $query);
 
-        // 检查查询是否成功
         if (!$result) {
             echo "<p>Error: " . mysqli_error($link) . "</p>";
         }
 
-        // 显示搜索结果
         if (mysqli_num_rows($result) > 0) {
             echo "<ul>";
             while ($row = mysqli_fetch_assoc($result)) {
@@ -59,7 +51,6 @@
         echo "<p>Please enter a keyword in the search box above.</p>";
     }
 
-    // 关闭数据库连接
     mysqli_close($link);
     ?>
 </body>
